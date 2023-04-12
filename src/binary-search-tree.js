17fb9694fa1a9befc2bfbb6f447612ebfeaa137f
aleksem07 <aleksem07@gmail.com>
2023-04-12 22:13:39 +0300

    feat: resolve add

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,37 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
-class BinarySearchTree {
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 
+class BinarySearchTree {
+  constructor() {
+    this.treeStart = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeStart;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeStart = add(this.treeStart, data);
+
+    function add(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+      if (node.data === data) {
+        return node;
+      }
+      if (data <= node.left) {
+        node.left = add(node.left, data);
+      } else {
+        node.left = add(node.right, data);
+      }
+      return node;
+    }
   }
 
   has(/* data */) {
@@ -25,7 +41,6 @@ class BinarySearchTree {
 
   find(/* data */) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   remove(/* data */) {
@@ -45,5 +60,5 @@ class BinarySearchTree {
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
